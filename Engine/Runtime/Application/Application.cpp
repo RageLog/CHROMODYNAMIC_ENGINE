a77463d7066d7f72ff1da47e83d5d282f29147c8
Ragelog <cemalltatli@gmail.com>
2022-01-28 04:29:33 +0300

    Development branch created

--- Engine/Runtime/Application/Application.cpp
+++ Engine/Runtime/Application/Application.cpp
@@ -5,40 +5,31 @@
 #include <ranges>
 #include <vector>
 
-using namespace CD;
-
-std::atomic<Application*> Application::m_instance;
-std::mutex Application::m_mutex;
-
-
-void Application::run(void)
+namespace CD
 {
-  
-  std::vector<int> numbers = { 1, 2, 3 ,4, 5 };
-  auto evenNumbers = numbers | std::ranges::views::filter([](int n){ return n % 2 == 0; })
-                           | std::ranges::views::transform([](int n) { return n * 2; });
-  for (auto &&i : evenNumbers)
+  std::atomic<Application*> Application::m_instance;
+  std::mutex Application::m_mutex;
+
+
+  void Application::run(void)
   {
-    std::cout<< i << "\n";
+    std::cout << ENGINE_VERSION << "\n";
   }
 
-  std::cout << ENGINE_VERSION << "\n";
   
-}
-
- 
-Application* Application::getInstance()
-{
-  Application* instance = m_instance.load(std::memory_order_acquire);
-  if (!instance)
+  Application* Application::getInstance()
   {
-    std::lock_guard<std::mutex> m_Lock(m_mutex);
-    instance = m_instance.load(std::memory_order_relaxed);
+    Application* instance = m_instance.load(std::memory_order_acquire);
     if (!instance)
     {
-      instance = new Application();
-      m_instance.store(instance, std::memory_order_release);
+      std::lock_guard<std::mutex> m_Lock(m_mutex);
+      instance = m_instance.load(std::memory_order_relaxed);
+      if (!instance)
+      {
+        instance = new Application();
+        m_instance.store(instance, std::memory_order_release);
+      }
     }
+    return instance;
   }
-  return instance;
 }
